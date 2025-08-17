e02ee8d4c300eb5a91029dde5e6b8721d1c5bc07
platinumbral <bradyln52901@gmail.com>
2025-08-16 23:45:50 -0500

    PT NPCs
    Added in basic NPCs throughout town. Mom gives inf money.

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1165,3 +1165,5 @@ EventScript_VsSeekerChargingDone::
 	.include "data/maps/PacifidlogTown_Pokemart/scripts.inc"
 
 	.include "data/maps/PacifidlogTown_ProfessorLab/scripts.inc"
+
+	.include "data/maps/PacifidlogTown_PlayerHouse/scripts.inc"
