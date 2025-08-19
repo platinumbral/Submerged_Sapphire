02042dd970ad41e27ddb727f3f6dfe59f5d8a3d6
platinumbral <bradyln52901@gmail.com>
2025-08-19 18:29:24 -0500

    Lab Update
    Added scripting for rival starter selection.
    
    Need to add scripting for talking to other NPCs depending on Pacifidlog_State

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -46,7 +46,7 @@
 #define FLAG_LEFT_STARTER    0x20 // Unused Flag
 #define FLAG_MID_STARTER     0x21 // Unused Flag
 #define FLAG_RIGHT_STARTER   0x22 // Unused Flag
-#define FLAG_UNUSED_0x023    0x23 // Unused Flag
+#define FLAG_UNUSED_0x23     0x23 // Unused Flag
 #define FLAG_UNUSED_0x024    0x24 // Unused Flag
 #define FLAG_UNUSED_0x025    0x25 // Unused Flag
 #define FLAG_UNUSED_0x026    0x26 // Unused Flag
